ae7b80e1dc531c7af580d83a28f1af3be594be0f
Floriano Albertini <floriano.webdev@gmail.com>
2024-08-05 15:42:55 +0200

    Backup

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: albertini <albertini@student.42.fr>        +#+  +:+       +#+        */
+/*   By: falberti <falberti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 15:57:39 by avdylavduli       #+#    #+#             */
-/*   Updated: 2024/07/30 14:07:37 by albertini        ###   ########.fr       */
+/*   Updated: 2024/08/05 15:40:44 by falberti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,6 +82,7 @@ typedef struct s_data
 	int		stdout;
 	int		pipe;
 	int		out;
+	bool	output;
 }	t_data;
 
 //init_structs
@@ -128,18 +129,21 @@ char	*ft_strndup(const char *s, size_t n);
 void	count_exit(int *nb, char **str, t_data *data);
 
 //Builtins
+void	ft_str_exit(char *str, int exit, t_data *data);
 void	ft_env(t_data *data);
 void	ft_cd(char **cmd, t_data *data);
-void	ft_pwd(char **cmd);
-void	ft_echo(char **cmd);
+void	ft_pwd(char **cmd, t_data *d);
+void	ft_echo(char **cmd, t_data *d);
 void	ft_cmd(char **cmd, t_data *data);
 void	ft_export(char **cmd, t_data *data);
 void	ft_unset(char **cmd, t_data *data);
 void	ft_mshell(t_data *data, char **cmd);
 
 //Exec
-char	**creat_tab(t_data *data, char **cmd);
+char	***creat_tab(t_data *data);
 void	ft_execute(char **cmd, t_data *data);
+void	ft_launch(t_data *data, char **cmd);
+char	*find_path(char *cmd, char **envp);
 
 //safe_functions
 void	ft_reset_std(t_data *data);
@@ -151,18 +155,21 @@ void	safe_pipe(int pipefd[2]);
 void	free_list(char **list);
 void	free_cmd(t_cmd *head);
 void	free_all(t_data *data);
-void	free_tab(char **tab);
+void	free_tab(char ***tab);
 
 //lst_utils
 //t_cmd	*create_new_node(char *str);
 void	ft_read_lst(t_data *data);
 int		lst_cmd_size(t_data *data);
+int		count_pipe(t_data *data);
+int		count_cmd(t_cmd *current);
 
 //redirection
-void	execute_pipeline(t_data *data, char **cmd);
-void	execute_redir(t_data *data, char **cmd);
-void	check_redir(t_data *data, char **cmd);
+void	execute_pipeline(t_data *data, char ***cmd);
+void	execute_redir(t_data *data);
 void	ft_reset_std(t_data *data);
+void	ft_stdin(t_data *data);
+void	ft_stdout(t_data *data);
 
 //exit
 int		is_exit(char *str, t_data *data);
